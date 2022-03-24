b70622c0632993f0cc78900998ac70e71cf53dd1
Dodi55 <65507291+Dodi55@users.noreply.github.com>
2022-03-24 15:32:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+
+	$arr[11] = 1;
+	$arr[22] = 6;
+	echo '<pre>';
+	print_r($arr);
+	echo '</pre>';
+
+
+?>
+
+</body>
+</html>
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
